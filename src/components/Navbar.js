a46ca7189b4fa7cb57b70666cb524b0ec9fbd61f
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2022-12-24 23:19:20 +0700

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { Component } from "react";
+import "./Navbar.Styles.css";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav classname="NavbarItems">
+        <h1 className="navbar-logo">Judul</h1>
+
+        <ul className="nav-menu">
+          <a href="/">
+            <i className="fa-solid fa-house-user"></i>Home
+          </a>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
